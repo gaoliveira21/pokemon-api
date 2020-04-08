8e3beb95e5d996d6fc4c0971ccb2009d648a4266
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-04-08 15:42:22 -0300

    apidoc sessions

--- docs/apidoc/api_project.js
+++ docs/apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-08T01:42:11.961Z",
+    "time": "2020-04-08T18:27:40.640Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,7 @@ const routes = new Router();
 const uploads = multer(multerConfig);
 
 /**
- * @api {post} /users
+ * @api {post} /users Create user
  * @apiName CreateUsers
  * @apiGroup Users
  *
@@ -30,7 +30,7 @@ const uploads = multer(multerConfig);
  *  }
  *
  * @apiSuccess (Success 201) {String} _id User ID
- * @apiSuccess (Success 201) {String} email E-mail of the (example@example.com)
+ * @apiSuccess (Success 201) {String} email E-mail of the user (example@example.com)
  * @apiSuccess (Success 201) {String} name  Fullname of the user
  * @apiSuccess (Success 201) {Boolean} success
  *
@@ -46,7 +46,7 @@ const uploads = multer(multerConfig);
  *  }
  *
  * @apiError (Error 400) UserExists The email sent already exists in database
- * @apiError (Error 400) InvalidParams Invalid params sent
+ * @apiError (Error 400) ValidationError Invalid params sent
  *
  * @apiErrorExample {json} UserExists:
  *  HTTP/1.1 400 Bad Request
@@ -55,7 +55,7 @@ const uploads = multer(multerConfig);
  *    "error": "User already exists"
  *  }
  *
- * @apiErrorExample {json} InvalidParams:
+ * @apiErrorExample {json} ValidationError:
  * HTTP/1.1 400 Bad Request
  * {
  *    "success": false,
@@ -66,8 +66,67 @@ const uploads = multer(multerConfig);
  * }
  *
  */
-
 routes.post('/users', UserController.store);
+
+/**
+ * @api {post} /sessions Start session
+ * @apiName StartSession
+ * @apiGroup Users
+ *
+ * @apiParam {String} email E-mail used on register
+ * @apiParam {String} password Password used on register
+ *
+ * @apiParamExample {json} Request-Example:
+ * {
+ *    email: "user@mail.com",
+ *    password: "123456"
+ * }
+ *
+ * @apiSuccess (Success 201) {String} _id User ID
+ * @apiSuccess (Success 201) {String} email User e-mail
+ * @apiSuccess (Success 201) {String} name Fullname of the user
+ * @apiSuccess (Success 201) {String} token JWT Token
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 201 Created
+ * {
+ *    "user": {
+ *      "_id": "5e8c85cdb62623882770849e",
+ *      "name": "User",
+ *      "email": "user@mail.com"
+ *    },
+ *    "token": "eyJhbGcTOIJIUzI1NiIsInR5cCI6IkpXVCJ8.eyJpZCI6IjVlOGM4NWNkYjYyNjIzMDAyNzcwODQ2ZSIsImlhdCI6MTU4NjM2ODM0MSwiZXhwIjoxNTg2OTczMTQxfQ.hSRy7GFR3e3mFC3FTXom6wNA7ufsCO_KDLRtB29bxzQ"
+ * }
+ *
+ * @apiError (Error 401) UserNotFound not found a user with email sent
+ * @apiError (Error 401) InvalidPassword password sent does not match with password in database
+ * @apiError (Error 400) ValidationError Invalid params sent
+ *
+ * @apiErrorExample {json} UserNotFound:
+ *  HTTP/1.1 401 Unauthorized
+ * {
+ *    "success": false,
+ *    "error": "User not found"
+ * }
+ *
+ * @apiErrorExample {json} InvalidPassword:
+ *  HTTP/1.1 401 Unauthorized
+ * {
+ *    "success": false,
+ *    "error": "Password does not match"
+ * }
+ *
+ * @apiErrorExample {json} ValidationError:
+ *  HTTP/1.1 401 Unauthorized
+ * {
+ *    "success": false,
+ *    "error": "ValidationError",
+ *    "details": [
+ *      "password is a required field"
+ *    ]
+ * }
+ *
+ */
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewares);
